9a5b8ba09827f04db7372fe236ac8c4d87088639
jihoon <znftm97@naver.com>
2020-08-05 15:29:08 +0900

    Material UI design

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,113 @@
 import React, { Component } from 'react';
-import CustomerAdd from './components/CustomerAdd';
+import Customer from './components/Customer'
 import './App.css';
-import Customer from './components/Customer';
-import Paper from '@material-ui/core/Paper';
+import Paper from '@material-ui/core/Paper'; // material-ui/라이브러리명/   , 라이브러리 설치 npm install --save @material-ui/라이브러리명
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { withStyles } from '@material-ui/core/styles'; 
-
+import { withStyles } from '@material-ui/core/styles';
+import CustomerAdd from './components/CustomerAdd';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 
 const styles = theme => ({
- root: {
-  width : '100%',
-  overflowX: 'auto',
- },
- table: {
-  minWidth: 1080,
- },
- progress:{
-   margin: theme.spacing.unit * 2
- }
-})
+   root: {
+      width: "100%",
+      minWidth: 1080
+},
+   menu: {
+      marginTop: 15,
+      marginBottom: 15,
+      display: 'flex',
+      justifyContent: 'center'
+},
+   paper: {
+      marginLeft: 18,
+      marginRight: 18
+},
+   progress: {
+      margin: theme.spacing.unit * 2
+},
+   grow: {
+      flexGrow: 1,
+},
+   tableHead: {
+      fontSize: '1.0rem'
+},
+   menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+},
+   title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+      display: 'block',
+   }
+},
+   search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+         backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+         marginLeft: theme.spacing.unit,
+         width: 'auto',
+      },
+},
+   searchIcon: {
+      width: theme.spacing.unit * 9,
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+},
+   inputRoot: {
+      color: 'inherit',
+      width: '100%',
+},
+   inputInput: {
+      paddingTop: theme.spacing.unit,
+      paddingRight: theme.spacing.unit,
+      paddingBottom: theme.spacing.unit,
+      paddingLeft: theme.spacing.unit * 10,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+         width: 120,
+         '&:focus': {
+            width: 200,
+          },
+      },
+}
+});
 
-class App extends React.Component{
+class App extends Component {
 
- constructor(props) {
+constructor(props) {
    super(props);
    this.state = {
-      customers: '',
-      completed: 0
+    customers: '',
+    completed: 0
    }
    this.stateRefresh = this.stateRefresh.bind(this);
 }
 
-   //고객 추가 시 전체페이지가 아닌 테이블만 새로고침 하기 위한 함수  
-stateRefresh() {
+stateRefresh() { // 전체 페이지가 아닌 테이블만 새로고침
    this.setState({
       customers: '',
       completed: 0
@@ -47,58 +117,85 @@ stateRefresh() {
       .catch(err => console.log(err));
 }
 
- componentDidMount(){
-    this.timer = setInterval(this.progress, 20);
-    this.callApi()
-      .then(res=> this.setState({customers: res}))
+componentDidMount() {
+   this.timer = setInterval(this.progress, 20);
+   this.callApi()
+      .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
- }
-
- callApi= async () =>{
-    const response = await fetch('api/customers');
-    const body = await response.json();
-    return body;
- }
-
- // DB에서 데이터 가져오는 동안 테이블에 프로그레스바를 출력
- progress = () => {
-    const {completed} = this.state;
-    this.setState({completed: completed >=100 ? 0 : completed +1});
- }
-
- render(){
-  const {classes} = this.props;
-  return( 
-   <div> 
-   <Paper className={classes.root}>
-​    <Table className={classes.table}>
-​     <TableHead>
-​      <TableRow>
-​       <TableCell>번호</TableCell>
-​       <TableCell>이미지</TableCell>
-​       <TableCell>이름</TableCell>
-​       <TableCell>생년월일</TableCell>
-​       <TableCell>성별</TableCell>
-​       <TableCell>직업</TableCell>
-       <TableCell>설정</TableCell>
-​      </TableRow>
-​     </TableHead>
-​     <TableBody>
-​      {this.state.customers ? this.state.customers.map(c =>{ 
-         return <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birth={c.birth} gender={c.gender} job={c.job}/>
-      }) :
-      <TableRow>
-         <TableCell colSpan="6" align="center">
-            <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-         </TableCell>
-      </TableRow>
-      }
-​     </TableBody>
-​    </Table>
-   </Paper>
-   <CustomerAdd stateRefresh={this.stateRefresh}/>
-   </div> 
-  )
- }
 }
+
+componentWillUnmount() {
+   clearInterval(this.timer);
+}
+
+callApi = async () => {
+   const response = await fetch('/api/customers');
+   const body = await response.json();
+   return body;
+}
+
+progress = () => { // 서버에서 데이터 가져오는게 오래 걸릴 때 표시하기위한 프로그레스 바
+   const { completed } = this.state;
+   this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+};
+
+render() {
+   const { classes } = this.props;
+   const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "성별", "직업", "설정"]
+   return (
+      <div className={classes.root}>
+         <AppBar position="static">
+            <Toolbar>
+               <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+                  <MenuIcon />
+               </IconButton>
+               <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                  고객 관리 시스템
+               </Typography>
+               <div className={classes.grow} />
+               <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                     <SearchIcon />
+                  </div>
+                  <InputBase
+                  placeholder="검색하기"
+                  classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                  }}
+                  />
+               </div>
+            </Toolbar>
+         </AppBar>
+         <div className={classes.menu}>
+            <CustomerAdd stateRefresh={this.stateRefresh} />
+         </div>
+         <Paper className={classes.paper}>
+            <Table>
+               <TableHead>
+                  <TableRow>
+                     {cellList.map(c => {
+                     return <TableCell className={classes.tableHead}>{c}</TableCell>
+                     })}
+                  </TableRow>
+                  </TableHead>
+                  <TableBody>
+                     {this.state.customers ?
+                        this.state.customers.map(c => {
+                        return <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+                        }) :
+                        <TableRow>
+                           <TableCell colSpan="6" align="center">
+                              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                           </TableCell>
+                        </TableRow>
+                     }
+                  </TableBody>
+            </Table>
+         </Paper>
+      </div>
+);
+}
+}
+
 export default withStyles(styles)(App);
--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import { TableBody } from '@material-ui/core';
-import CustomerDelete from './CustomerDelete';
+import CustomerDelete from './CustomerDelete'
 
 class Customer extends React.Component {
-  render(){
-    return(
-      <TableRow>
-      ​ <TableCell>{this.props.id}</TableCell>
-      ​ <TableCell><img src={this.props.image} alt='profile' style={{width:64,height:64}}/></TableCell>
-      ​ <TableCell>{this.props.name}</TableCell>
-      ​ <TableCell>{this.props.birth}</TableCell>
-      ​ <TableCell>{this.props.gender}</TableCell>
-      ​ <TableCell>{this.props.job}</TableCell>
-       <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id}/></TableCell>
-      ​</TableRow>
+  render() {
+  return (
+    <TableRow>
+      <TableCell>{this.props.id}</TableCell>
+      <TableCell><img src={this.props.image} alt="profile" style={{width:64,height:64}}/></TableCell>
+      <TableCell>{this.props.name}</TableCell>
+      <TableCell>{this.props.birthday}</TableCell>
+      <TableCell>{this.props.gender}</TableCell>
+      <TableCell>{this.props.job}</TableCell>
+      <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id}/></TableCell>
+    </TableRow>
     )
-  }  
+  }
 }
+
 export default Customer;
